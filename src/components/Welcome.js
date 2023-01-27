5fc3c68b75d3841784dfb025a42715a7b3c79705
samouchka1 <mchio421@protonmail.com>
2023-01-26 23:27:15 -0500

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,13 @@ import {
     Box,
     Paper,
     Typography,
-    Divider
+    Divider,
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableCell,
+    TableRow
 } from '@mui/material';
 import { Slide } from "react-awesome-reveal";
 import { 
@@ -31,8 +37,10 @@ ChartJS.register(
   LineElement,
   Title, 
   Tooltip, 
-  Legend
+  Legend,
 );
+
+
 
 export const options = {
   responsive: true,
@@ -57,7 +65,9 @@ export const lineData = {
       label: '',
       data: labels.map(() => faker.datatype.number({ min: 1000, max: 1500 })),
       borderColor: '#299526',
-      backgroundColor: '#20761d',
+      backgroundColor: '#00000000',
+      pointBorderWidth: 0,
+      pointBorderColor: '#00000000',
     }
   ],
 };
@@ -79,6 +89,7 @@ export const pieData = {
   ],
 };
 
+
 const watchListPercentChangeStyles = {
   color: 'success.main'
 }
@@ -96,8 +107,39 @@ const watchListItemStyles = {
 
 const eventsAreaStyles = {
   p: '.25rem 1rem',
-  m: '.25rem 1rem'
+  m: '.35rem 1rem'
 }
+
+const economicEvents = [
+
+  {
+    event: 'FOMC Meeting',
+    date: '2/1/2023',
+  },
+  {
+    event: 'AMZN Earnings',
+    date: '2/3/2023',
+  },
+  {
+    event: 'CPI Report',
+    date: '2/14/2023',
+  },
+  {
+    event: 'Employment Data',
+    date: '3/2/2023',
+  },
+]
+
+function createData(ticker, type, amount, date) {
+  return { ticker, type, amount, date };
+}
+
+const rows = [
+  createData('SPX', 'BTO', 200, '1/20/2023'),
+  createData('AMZN', 'BTO', 100, '12/7/2022'),
+  createData('GC', 'BTO', 200, '11/20/2022'),
+  createData('TSLA', 'STC', -100, '9/14/2022'),
+];
 
 const Welcome = () => {
   return (
@@ -112,7 +154,7 @@ const Welcome = () => {
             }}
             elevation="5"
           >
-            <Typography variant="h3" sx={{textAlign: 'center', fontFamily: 'Teko'}}>
+            <Typography variant="h3" sx={{textAlign: 'center', fontFamily: 'Teko', caretColor: 'transparent'}}>
               Dashboard
             </Typography>
 
@@ -156,7 +198,34 @@ const Welcome = () => {
                     }
                   }}
                 >
+
                   <Line options={options} data={lineData} />
+
+                  <Box sx={{display: 'flex', flexDirection:'column', justifyContent: 'space-between', m: 'auto'}}>
+                    <Typography sx={{mt:'1rem'}}>Trade Log</Typography>
+                    <TableContainer>
+                      <Table>
+                        <TableHead>
+                          <TableRow sx={{fontWeight: '600'}}>
+                            <TableCell align="left">Ticker</TableCell>
+                            <TableCell align="left">Type</TableCell>
+                            <TableCell align="right">Amount</TableCell>
+                            <TableCell align="right">Date</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {rows.map((row) => (
+                            <TableRow key={row}>
+                              <TableCell align="left">{row.ticker}</TableCell>
+                              <TableCell align="left">{row.type}</TableCell>
+                              <TableCell align="right">{row.amount}</TableCell>
+                              <TableCell align="right">{row.date}</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Box>
                 </Paper>
               </Box>
 
@@ -174,20 +243,17 @@ const Welcome = () => {
                       boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)'
                     }
                   }}>
-                  <Box sx={eventsAreaStyles}>
-                    <Typography>AMZN Earnings Report</Typography>
-                  </Box>
-                  <Box sx={eventsAreaStyles}>
-                    <Typography>FOMC Meeting</Typography>
-                  </Box>
-                  <Box sx={eventsAreaStyles}>
-                    <Typography>CPI Report</Typography>
-                  </Box>
-                  <Box sx={eventsAreaStyles}>
-                    <Typography>Employment Data</Typography>
-                  </Box>
+
+                    {economicEvents.map((day)=> (
+                      <Box key={day} sx={eventsAreaStyles}>
+                        <Typography>{day.event}</Typography>
+                        <Typography variant="subtitle" sx={{fontSize: '.9rem'}}>{day.date}</Typography>
+                      </Box>
+                    ))}
+                  
                 </Paper>
               </Box>
+
               <Divider sx={{ display: {md: 'none', xs: 'block'}}} />
 
               <Box>
@@ -200,7 +266,7 @@ const Welcome = () => {
                     gap: '.25rem'
                   }}
                 >
-                  {['SPX', 'VIX', 'AMZN', 'GC', 'TNX'].map((item) => (
+                  {['SPX', 'VIX', 'AMZN', 'GC', 'CL', 'ZW', 'TNX'].map((item) => (
                   <Paper key={item} sx={watchListItemStyles}>
                     <Typography>{item}</Typography>
                     <Typography sx={watchListPercentChangeStyles}>
